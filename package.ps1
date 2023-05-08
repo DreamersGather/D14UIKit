eabb8270e68f3b9d4aade6512ee2d4b847ce0d46
yiyaowen <wenyiyao_fort@163.com>
2023-05-08 21:40:13 +0800

    Adjust package destination

--- package.ps1
+++ package.ps1
@@ -1,23 +1,23 @@
 ﻿param($version)
 
-Remove-Item -Recurse -Force Wrap | Out-Null
+Remove-Item -Recurse -Force Out | Out-Null
 
-New-Item -Path 'Wrap\cpp\include' -ItemType 'directory' | Out-Null
-Copy-Item -Recurse -Path 'Exp\Inc\*' -Destination 'Wrap\cpp\include' | Out-Null
-New-Item -Path 'Wrap\cpp\lib\debug' -ItemType 'directory' | Out-Null
-New-Item -Path 'Wrap\cpp\lib\release' -ItemType 'directory' | Out-Null
-Copy-Item 'x64\Rebug\D14UIKit.dll' -Destination 'Wrap\cpp\lib\debug' | Out-Null
-Copy-Item 'x64\Release\D14UIKit.lib' -Destination 'Wrap\cpp\lib' | Out-Null
-Copy-Item 'x64\Release\D14UIKit.dll' -Destination 'Wrap\cpp\lib\release' | Out-Null
-Compress-Archive -Force -Path 'Wrap\cpp' -DestinationPath "Wrap\d14uikit_cpp_$version.zip" | Out-Null
+New-Item -Path 'Out\cpp\include' -ItemType 'directory' | Out-Null
+Copy-Item -Recurse -Path 'Exp\Inc\*' -Destination 'Out\cpp\include' | Out-Null
+New-Item -Path 'Out\cpp\lib\debug' -ItemType 'directory' | Out-Null
+New-Item -Path 'Out\cpp\lib\release' -ItemType 'directory' | Out-Null
+Copy-Item 'x64\Rebug\D14UIKit.dll' -Destination 'Out\cpp\lib\debug' | Out-Null
+Copy-Item 'x64\Release\D14UIKit.lib' -Destination 'Out\cpp\lib' | Out-Null
+Copy-Item 'x64\Release\D14UIKit.dll' -Destination 'Out\cpp\lib\release' | Out-Null
+Compress-Archive -Force -Path 'Out\cpp' -DestinationPath "Out\d14uikit_cpp_$version.zip" | Out-Null
 
-Write-Output "C++ wrapper packaged to Wrap\d14uikit_cpp_$version.zip"
+Write-Output "C++ wrapper packaged to Out\d14uikit_cpp_$version.zip"
 
-New-Item -Path 'Wrap\python' -ItemType 'directory' | Out-Null
-Copy-Item 'x64\RPyBind\D14UIKit.pyd' -Destination 'Wrap\python' | Out-Null
-Set-Location 'Wrap\python' | Out-Null
+New-Item -Path 'Out\python' -ItemType 'directory' | Out-Null
+Copy-Item 'x64\RPyBind\D14UIKit.pyd' -Destination 'Out\python' | Out-Null
+Set-Location 'Out\python' | Out-Null
 &'stubgen' '-m' 'D14UIKit' '-o' '.' | Out-Null
 Set-Location '..\..' | Out-Null # fucking mypy stubgen, --search-path is a bullshit
-Compress-Archive -Force -Path 'Wrap\python' -DestinationPath "Wrap\d14uikit_python_$version.zip" | Out-Null
+Compress-Archive -Force -Path 'Out\python' -DestinationPath "Out\d14uikit_python_$version.zip" | Out-Null
 
-Write-Output "Python wrapper packaged to Wrap\d14uikit_python_$version.zip"
+Write-Output "Python wrapper packaged to Out\d14uikit_python_$version.zip"
