87899d70f5a517770e11d32f7091ebc991edfb3e
yiyaowen <wenyiyao_fort@163.com>
2023-05-11 14:42:26 +0800

    Update demos

---
+++ Test/DemoTemplate.cpp
@@ -0,0 +1,24 @@
+﻿#include "Application.h"
+#include "MainWindow.h"
+
+using namespace d14uikit;
+
+#define DEMO_NAME L"DemoTemplate"
+
+int main(int argc, char* argv[])
+{
+    float dpi = 96.0f;
+    if (argc >= 2 && strcmp(argv[1], "HighDPI"))
+    {
+        dpi = 192.0f;
+    }
+    Application app(DEMO_NAME, dpi);
+
+    //------------------------------------------- Initialize UI objects.
+
+    MainWindow mwnd;
+
+    //------------------------------------------- Set UI event callacks.
+
+    return app.run();
+}
--- package.ps1
+++ package.ps1
@@ -9,6 +9,9 @@ New-Item -Path 'Out\cpp\lib\release' -ItemType 'directory' | Out-Null
 Copy-Item 'x64\Rebug\D14UIKit.dll' -Destination 'Out\cpp\lib\debug' | Out-Null
 Copy-Item 'x64\Release\D14UIKit.lib' -Destination 'Out\cpp\lib' | Out-Null
 Copy-Item 'x64\Release\D14UIKit.dll' -Destination 'Out\cpp\lib\release' | Out-Null
+New-Item -Path 'Out\cpp\demo' -ItemType 'directory' | Out-Null
+Copy-Item -Recurse -Path 'Test\*.cpp' -Destination 'Out\cpp\demo' | Out-Null
+Copy-Item 'Test\test.png' -Destination 'Out\cpp\demo' | Out-Null
 Compress-Archive -Force -Path 'Out\cpp' -DestinationPath "Out\d14uikit_cpp_$version.zip" | Out-Null
 
 Write-Output "C++ wrapper packaged to Out\d14uikit_cpp_$version.zip"
@@ -18,6 +21,9 @@ Copy-Item 'x64\RPyBind\D14UIKit.pyd' -Destination 'Out\python' | Out-Null
 Set-Location 'Out\python' | Out-Null
 &'stubgen' '-m' 'D14UIKit' '-o' '.' | Out-Null
 Set-Location '..\..' | Out-Null # fucking mypy stubgen, --search-path is a bullshit
+New-Item -Path 'Out\python\demo' -ItemType 'directory' | Out-Null
+Copy-Item -Recurse -Path 'Test\PyBind\*.py' -Destination 'Out\python\demo' | Out-Null
+Copy-Item 'Test\test.png' -Destination 'Out\python\demo' | Out-Null
 Compress-Archive -Force -Path 'Out\python' -DestinationPath "Out\d14uikit_python_$version.zip" | Out-Null
 
 Write-Output "Python wrapper packaged to Out\d14uikit_python_$version.zip"
