4012ce9e117f9b854e75338a50e5820816e55361
yiyaowen <wenyiyao_fort@163.com>
2023-07-02 16:25:24 +0800

    Fix PyBind demo bugs

--- Test/PyBind/AdvancedCallback.py
+++ Test/PyBind/AdvancedCallback.py
@@ -6,7 +6,7 @@ DEMO_NAME = 'AdvancedCallback'
 
 if __name__ == '__main__':
     dpi = 96.0
-    if len(argv) >= 2:
+    if len(argv) >= 2 and argv[1] == 'HighDPI':
         dpi = 192.0
 
     app = Application(DEMO_NAME, dpi)
