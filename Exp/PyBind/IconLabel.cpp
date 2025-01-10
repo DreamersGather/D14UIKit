9fa9f488d3c5e0b5851a8c66a4db2b307e5c1ba2
yiyaowen <wenyiyao_fort@163.com>
2025-01-10 19:03:56 +0800

    Add missing PyBind::IconLabel

---
+++ Exp/PyBind/IconLabel.cpp
@@ -0,0 +1,37 @@
+﻿#include "Common/Precompile.h"
+
+#include "IconLabel.h"
+
+#include "Panel.h"
+
+#include "Inc/IconLabel.h"
+#include "Inc/Image.h"
+#include "Inc/Label.h"
+
+namespace d14uikit
+{
+    void Py_InitIconLabel(py::module_& m)
+    {
+        py::class_<IconLabel, Panel, PyPanel<IconLabel>> i(m, "IconLabel");
+
+        i.def(py::init<const std::wstring&>(), "text"_a = L"");
+
+        i.def_property(
+            "icon",
+            &IconLabel::icon,
+            &IconLabel::setIcon);
+
+        i.def_property(
+            "iconSize",
+            &IconLabel::iconSize,
+            &IconLabel::setIconSize);
+
+        i.def_property_readonly(
+            "label",
+            &IconLabel::label);
+
+        i.def(
+            "updateLayout",
+            &IconLabel::updateLayout);
+    }
+}
