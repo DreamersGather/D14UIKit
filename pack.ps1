0f9fac9ccd7d8d621ee7ec43f911a896c4fc92be
yiyaowen <wenyiyao_fort@163.com>
2025-02-15 02:20:11 +0800

    Update

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,10 @@
 ﻿param($version)
 
-function D14UIKit-Remove-Expired
+######################
+# Clear Expired Dist #
+######################
+
+function D14UIKit-Clear-Expired
 {
     param($DistName)
     if (Test-Path -Path "Dist\\$DistName")
@@ -8,18 +12,15 @@ function D14UIKit-Remove-Expired
         Remove-Item -Recurse -Force "Dist\\$DistName" | Out-Null
     }
 }
-D14UIKit-Remove-Expired -DistName "cpp-x86"
-D14UIKit-Remove-Expired -DistName "cpp-x64"
+D14UIKit-Clear-Expired -DistName "cpp-x86"
+D14UIKit-Clear-Expired -DistName "cpp-x64"
 
-D14UIKit-Remove-Expired -DistName "py310-x86"
-D14UIKit-Remove-Expired -DistName "py311-x86"
-D14UIKit-Remove-Expired -DistName "py312-x86"
-D14UIKit-Remove-Expired -DistName "py313-x86"
+D14UIKit-Clear-Expired -DistName "python-x86"
+D14UIKit-Clear-Expired -DistName "python-x64"
 
-D14UIKit-Remove-Expired -DistName "py310-x64"
-D14UIKit-Remove-Expired -DistName "py311-x64"
-D14UIKit-Remove-Expired -DistName "py312-x64"
-D14UIKit-Remove-Expired -DistName "py313-x64"
+#####################
+# Pack d14uikit_cpp #
+#####################
 
 function D14UIKit-Pack-Cpp
 {
@@ -35,13 +36,17 @@ function D14UIKit-Pack-Cpp
     Copy-Item "Out\\$ArchName\\Release\\D14UIKit.dll" -Destination "Dist\\cpp-$ArchName\\lib\\release" | Out-Null
     # example
     New-Item -Path "Dist\\cpp-$ArchName\\demo" -ItemType "directory" | Out-Null
-    Copy-Item -Recurse -Path "Test\\*.cpp" -Destination "Dist\\cpp-$ArchName\\demo" | Out-Null
     Copy-Item -Recurse -Path "Test\\images" -Destination "Dist\\cpp-$ArchName\\demo\\images" | Out-Null
+    Copy-Item -Recurse -Path "Test\\sources" -Destination "Dist\\cpp-$ArchName\\demo\\sources" | Out-Null
     # archive
     Compress-Archive -Force -Path "Dist\\cpp-$ArchName\\*" -DestinationPath "Dist\\d14uikit_cpp_$version-$ArchName.zip" | Out-Null
 }
 D14UIKit-Pack-Cpp -ArchName "x86"
 D14UIKit-Pack-Cpp -ArchName "x64"
+
+########################
+# Pack d14uikit_python #
+########################
 
 # Attempt to generate a stub file using mypy's stubgen.exe.
 $executablePath = Get-Command "stubgen.exe" -ErrorAction SilentlyContinue
@@ -58,26 +63,32 @@ else # If stubgen.exe is not found, generate a dummy stub file instead.
     New-Item -Path "Test\\PyBind\\D14UIKit.pyi" -ItemType "file" | Out-Null
 }
 
-function D14UIKit-Pack-Python
+function D14UIKit-Copy-Python
 {
     param($PyVerNum, $ArchName)
     # library
-    New-Item -Path "Dist\\py$PyVerNum-$ArchName" -ItemType "directory" | Out-Null
-    Copy-Item "Out\\$ArchName\\RPy$PyVerNum\\D14UIKit.pyd" -Destination "Dist\\py$PyVerNum-$ArchName" | Out-Null
-    Copy-Item "Test\\PyBind\\D14UIKit.pyi" -Destination "Dist\\py$PyVerNum-$ArchName" | Out-Null
+    New-Item -Path "Dist\\python-$ArchName\\py$PyVerNum-$ArchName" -ItemType "directory" | Out-Null
+    Copy-Item "Out\\$ArchName\\RPy$PyVerNum\\D14UIKit.pyd" -Destination "Dist\\python-$ArchName\\py$PyVerNum-$ArchName" | Out-Null
+    Copy-Item "Test\\PyBind\\D14UIKit.pyi" -Destination "Dist\\python-$ArchName\\py$PyVerNum-$ArchName" | Out-Null
     # example
-    New-Item -Path "Dist\\py$PyVerNum-$ArchName\\demo" -ItemType "directory" | Out-Null
-    Copy-Item -Recurse -Path "Test\\PyBind\\*.py" -Destination "Dist\\py$PyVerNum-$ArchName\\demo" | Out-Null
-    Copy-Item -Recurse -Path "Test\\images" -Destination "Dist\\py$PyVerNum-$ArchName\\demo\\images" | Out-Null
-    # archive
-    Compress-Archive -Force -Path "Dist\\py$PyVerNum-$ArchName\\*" -DestinationPath "Dist\\d14uikit_py$($PyVerNum)_$version-$ArchName.zip" | Out-Null
+    New-Item -Path "Dist\\python-$ArchName\\py$PyVerNum-$ArchName\\demo" -ItemType "directory" | Out-Null
+    Copy-Item -Recurse -Path "Test\\PyBind\\images" -Destination "Dist\\python-$ArchName\\py$PyVerNum-$ArchName\\demo\\images" | Out-Null
+    Copy-Item -Recurse -Path "Test\\PyBind\\scripts" -Destination "Dist\\python-$ArchName\\py$PyVerNum-$ArchName\\demo\\scripts" | Out-Null
 }
-D14UIKit-Pack-Python -PyVerNum "310" -ArchName "x86"
-D14UIKit-Pack-Python -PyVerNum "311" -ArchName "x86"
-D14UIKit-Pack-Python -PyVerNum "312" -ArchName "x86"
-D14UIKit-Pack-Python -PyVerNum "313" -ArchName "x86"
+function D14UIKit-Pack-Python
+{
+    param($ArchName)
+    Compress-Archive -Force -Path "Dist\\python-$ArchName\\*" -DestinationPath "Dist\\d14uikit_python_$version-$ArchName.zip" | Out-Null
+}
+D14UIKit-Copy-Python -PyVerNum "310" -ArchName "x86"
+D14UIKit-Copy-Python -PyVerNum "311" -ArchName "x86"
+D14UIKit-Copy-Python -PyVerNum "312" -ArchName "x86"
+D14UIKit-Copy-Python -PyVerNum "313" -ArchName "x86"
 
-D14UIKit-Pack-Python -PyVerNum "310" -ArchName "x64"
-D14UIKit-Pack-Python -PyVerNum "311" -ArchName "x64"
-D14UIKit-Pack-Python -PyVerNum "312" -ArchName "x64"
-D14UIKit-Pack-Python -PyVerNum "313" -ArchName "x64"
+D14UIKit-Copy-Python -PyVerNum "310" -ArchName "x64"
+D14UIKit-Copy-Python -PyVerNum "311" -ArchName "x64"
+D14UIKit-Copy-Python -PyVerNum "312" -ArchName "x64"
+D14UIKit-Copy-Python -PyVerNum "313" -ArchName "x64"
+
+D14UIKit-Pack-Python -ArchName "x86"
+D14UIKit-Pack-Python -ArchName "x64"
